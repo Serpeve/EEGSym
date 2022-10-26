5d43d948bb5e9ad99f9b2d680351238369ec525a
Sergio Pérez-Velasco <serpeve@gmail.com>
2022-10-26 09:52:04 +0200

    Solved problems of example code presented in the README.md file regarding issue #3 from Shamim Sasani.

--- EEGSym_architecture.py
+++ EEGSym_architecture.py
@@ -144,7 +144,6 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
                                  padding='valid')
             unit_batchconv_1 = BatchNormalization()
 
-        # Temporal analysis stage of the module
         for j in range(len(input)):
             block_side_units = list()
             for i in range(len(scales_samples)):
@@ -157,30 +156,31 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
 
                 block_side_units.append(unit)
             block_units.append(block_side_units)
-        # Concatenation of possible inception modules
+        # Concatenation
         block_out = list()
         for j in range(len(input)):
             if len(block_units[j]) != 1:
                 block_out.append(
                     keras.layers.concatenate(block_units[j], axis=-1))
-                block_out_temp = input[j]
             else:
                 block_out.append(block_units[j][0])
-                block_out_temp = input[j]
-                block_out_temp = unit_conv_1(block_out_temp)
-                block_out_temp = unit_batchconv_1(block_out_temp)
-                block_out_temp = Activation(activation)(block_out_temp)
-                block_out_temp = Dropout(dropout_rate)(block_out_temp)
 
             if residual:
+                if len(block_units[j]) != 1:
+                    block_out_temp = input[j]
+                else:
+                    block_out_temp = input[j]
+                    block_out_temp = unit_conv_1(block_out_temp)
+
+                    block_out_temp = unit_batchconv_1(block_out_temp)
+                    block_out_temp = Activation(activation)(block_out_temp)
+                    block_out_temp = Dropout(dropout_rate)(block_out_temp)
+
                 block_out[j] = Add()([block_out[j], block_out_temp])
-            else:
-                block_out[j] = block_out_temp
 
             if average != 1:
                 block_out[j] = AveragePooling3D((1, average, 1))(block_out[j])
 
-        # Spatial analysis stage of the module
         if ncha != 1:
             for i in range(spatial_resnet_repetitions):
                 block_out_temp = list()
@@ -222,7 +222,6 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
                             block_out[j] = Add()(
                                 [block_out[j], block_out_temp[j]])
         return block_out
-
     # ============================= CALCULATIONS ============================= #
     input_samples = int(input_time * fs / 1000)
     scales_samples = [int(s * fs / 1000) for s in scales_time]
@@ -247,8 +246,10 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
                 np.array(range(ncha - ch_lateral)) + ch_lateral)
             ch_central = tf.gather(input, indices=central_idx, axis=-2)
 
-            left_init = keras.layers.concatenate((ch_left, ch_central), axis=-2)
-            right_init = keras.layers.concatenate((ch_right, ch_central), axis=-2)
+            left_init = keras.layers.concatenate((ch_left, ch_central),
+                                                 axis=-2)
+            right_init = keras.layers.concatenate((ch_right, ch_central),
+                                                  axis=-2)
         else:
             left_init = ch_left
             right_init = ch_right
@@ -258,66 +259,65 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
     else:
         division = 1
     # ======================== TEMPOSPATIAL ANALYSIS ========================= #
-    # ======================================================================== #
-
     # ============================ Inception (x2) ============================ #
     b1_out = general_module([input],
-                                scales_samples=scales_samples,
-                                filters_per_branch=filters_per_branch,
-                                ncha=ncha,
-                                activation=activation,
-                                dropout_rate=dropout_rate, average=2,
-                                spatial_resnet_repetitions=spatial_resnet_repetitions,
-                                residual=residual, init=True)
+                              scales_samples=scales_samples,
+                              filters_per_branch=filters_per_branch,
+                              ncha=ncha,
+                              activation=activation,
+                              dropout_rate=dropout_rate, average=2,
+                              spatial_resnet_repetitions=spatial_resnet_repetitions,
+                              residual=residual, init=True)
 
     b2_out = general_module(b1_out, scales_samples=[int(x / 4) for x in
-                                                        scales_samples],
-                                filters_per_branch=filters_per_branch,
-                                ncha=ncha,
-                                activation=activation,
-                                dropout_rate=dropout_rate, average=2,
-                                spatial_resnet_repetitions=spatial_resnet_repetitions,
-                                residual=residual)
+                                                      scales_samples],
+                              filters_per_branch=filters_per_branch,
+                              ncha=ncha,
+                              activation=activation,
+                              dropout_rate=dropout_rate, average=2,
+                              spatial_resnet_repetitions=spatial_resnet_repetitions,
+                              residual=residual)
     # ============================== Residual (x3) =========================== #
     b3_u1 = general_module(b2_out, scales_samples=[16],
-                               filters_per_branch=int(
-                                   filters_per_branch * len(scales_samples) / 2),
-                               ncha=ncha,
-                               activation=activation,
-                               dropout_rate=dropout_rate, average=2,
-                               spatial_resnet_repetitions=spatial_resnet_repetitions,
-                               residual=residual)
+                             filters_per_branch=int(
+                                 filters_per_branch * len(
+                                     scales_samples) / 2),
+                             ncha=ncha,
+                             activation=activation,
+                             dropout_rate=dropout_rate, average=2,
+                             spatial_resnet_repetitions=spatial_resnet_repetitions,
+                             residual=residual)
     b3_u1 = general_module(b3_u1,
-                               scales_samples=[8],
-                               filters_per_branch=int(
-                                   filters_per_branch * len(
-                                       scales_samples) / 2),
+                             scales_samples=[8],
+                             filters_per_branch=int(
+                                 filters_per_branch * len(
+                                     scales_samples) / 2),
 
-                               ncha=ncha,
-                               activation=activation,
-                               dropout_rate=dropout_rate, average=2,
-                               spatial_resnet_repetitions=spatial_resnet_repetitions,
-                               residual=residual)
+                             ncha=ncha,
+                             activation=activation,
+                             dropout_rate=dropout_rate, average=2,
+                             spatial_resnet_repetitions=spatial_resnet_repetitions,
+                             residual=residual)
     b3_u2 = general_module(b3_u1, scales_samples=[4],
-                               filters_per_branch=int(
-                                   filters_per_branch * len(
-                                       scales_samples) / 4),
-                               ncha=ncha,
-                               activation=activation,
-                               dropout_rate=dropout_rate, average=2,
-                               spatial_resnet_repetitions=spatial_resnet_repetitions,
-                               residual=residual)
-
+                             filters_per_branch=int(
+                                 filters_per_branch * len(
+                                     scales_samples) / 4),
+                             ncha=ncha,
+                             activation=activation,
+                             dropout_rate=dropout_rate, average=2,
+                             spatial_resnet_repetitions=spatial_resnet_repetitions,
+                             residual=residual)
+    # ========================== TEMPORAL REDUCTION ========================== #
     t_red = b3_u2[0]
     for _ in range(1):
         t_red_temp = t_red
         t_red_temp = Conv3D(kernel_size=(1, 4, 1),
-                             filters=int(filters_per_branch * len(
-                                 scales_samples) / 4),
-                             use_bias=False,
-                             strides=(1, 1, 1),
-                             kernel_initializer='he_normal',
-                             padding='same')(t_red_temp)
+                            filters=int(filters_per_branch * len(
+                                scales_samples) / 4),
+                            use_bias=False,
+                            strides=(1, 1, 1),
+                            kernel_initializer='he_normal',
+                            padding='same')(t_red_temp)
         t_red_temp = BatchNormalization()(t_red_temp)
         t_red_temp = Activation(activation)(t_red_temp)
         t_red_temp = Dropout(dropout_rate)(t_red_temp)
@@ -330,18 +330,17 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
     t_red = AveragePooling3D((1, 2, 1))(t_red)
 
     # =========================== CHANNEL MERGING ============================ #
-    # ======================================================================== #
     ch_merg = t_red
     if residual:
         for _ in range(2):
             ch_merg_temp = ch_merg
             ch_merg_temp = Conv3D(kernel_size=(division, 1, ncha),
-                                 filters=int(filters_per_branch * len(
-                                     scales_samples) / 4),
-                                 use_bias=False,
-                                 strides=(1, 1, 1),
-                                 kernel_initializer='he_normal',
-                                 padding='valid')(ch_merg_temp)
+                                  filters=int(filters_per_branch * len(
+                                      scales_samples) / 4),
+                                  use_bias=False,
+                                  strides=(1, 1, 1),
+                                  kernel_initializer='he_normal',
+                                  padding='valid')(ch_merg_temp)
             ch_merg_temp = BatchNormalization()(ch_merg_temp)
             ch_merg_temp = Activation(activation)(ch_merg_temp)
             ch_merg_temp = Dropout(dropout_rate)(ch_merg_temp)
@@ -349,10 +348,12 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
             ch_merg = Add()([ch_merg, ch_merg_temp])
 
         ch_merg = Conv3D(kernel_size=(division, 1, ncha),
-                        filters=int(filters_per_branch * len(scales_samples) / 4),
-                        groups=int(filters_per_branch * len(scales_samples) / 8),
-                        use_bias=False,
-                        padding='valid')(ch_merg)
+                         filters=int(
+                             filters_per_branch * len(scales_samples) / 4),
+                         groups=int(
+                             filters_per_branch * len(scales_samples) / 8),
+                         use_bias=False,
+                         padding='valid')(ch_merg)
         ch_merg = BatchNormalization()(ch_merg)
         ch_merg = Activation(activation)(ch_merg)
         ch_merg = Dropout(dropout_rate)(ch_merg)
@@ -360,16 +361,17 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
         if symmetric:
             ch_merg = Conv3D(kernel_size=(division, 1, 1),
                              filters=int(
-                                 filters_per_branch * len(scales_samples) / 4),
+                                 filters_per_branch * len(
+                                     scales_samples) / 4),
                              groups=int(
-                                 filters_per_branch * len(scales_samples) / 8),
+                                 filters_per_branch * len(
+                                     scales_samples) / 8),
                              use_bias=False,
                              padding='valid')(ch_merg)
             ch_merg = BatchNormalization()(ch_merg)
             ch_merg = Activation(activation)(ch_merg)
             ch_merg = Dropout(dropout_rate)(ch_merg)
     # ========================== TEMPORAL MERGING ============================ #
-    # ======================================================================== #
     t_merg = ch_merg
     for _ in range(1):
         if residual:
@@ -401,8 +403,10 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
             t_merg = t_merg_temp
 
     t_merg = Conv3D(kernel_size=(1, input_samples // 64, 1),
-                    filters=int(filters_per_branch * len(scales_samples) / 4) * 2,
-                    groups=int(filters_per_branch * len(scales_samples) / 4),
+                    filters=int(
+                        filters_per_branch * len(scales_samples) / 4) * 2,
+                    groups=int(
+                        filters_per_branch * len(scales_samples) / 4),
                     use_bias=False,
                     padding='valid')(t_merg)
     t_merg = BatchNormalization()(t_merg)
@@ -413,7 +417,9 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
     for _ in range(4):
         output_temp = output
         output_temp = Conv3D(kernel_size=(1, 1, 1),
-                             filters=int(filters_per_branch * len(scales_samples) / 2),
+                             filters=int(
+                                 filters_per_branch * len(
+                                     scales_samples) / 2),
                              use_bias=False,
                              strides=(1, 1, 1),
                              kernel_initializer='he_normal',
@@ -427,12 +433,11 @@ def EEGSym(input_time=3000, fs=128, ncha=8, filters_per_branch=8,
             output = output_temp
     output = Flatten()(output)
     output_layer = Dense(n_classes, activation='softmax')(output)
-
-    # ======================================================================== #
     # Create and compile model
     model = keras.models.Model(inputs=input_layer, outputs=output_layer)
     optimizer = keras.optimizers.Adam(learning_rate=learning_rate,
-                                      beta_1=0.9, beta_2=0.999, amsgrad=False)
+                                      beta_1=0.9, beta_2=0.999,
+                                      amsgrad=False)
     model.compile(loss='categorical_crossentropy', optimizer=optimizer,
                   metrics=['accuracy'])
     return model
--- README.md
+++ README.md
@@ -10,45 +10,61 @@ Intersubject Variability in Motor Imagery Based BCIs With Deep Learning".
 from EEGSym_architecture import EEGSym
 from EEGSym_DataAugmentation import trial_iterator
 from tensorflow.keras.callbacks import EarlyStopping as kerasEarlyStopping
+from tensorflow.keras.utils import to_categorical
+import numpy as np
 
+#%% Load signal and model
 # Batch size used for fine-tuning
 bs_EEGSym = 32  # bs_EEGSym = 256 for pre-training (To reduce compute time)
 
-# Load the X EEG features and Y labels of your choiche in the followin format:
-# X = Features in the form of [examples, samples, ncha, 1]
-# Y = one_hot encoded labels, i.e., [examples, 1, 1]
-
-# Divide the features into training, validation and test to obtain:
-# X_train, Y_train, X_validate, Y_validate, X_test, Y_test
-
-# Select if the pre-trained weight values on 5 datasets that include 280  
+# Select if the pre-trained weight values on 5 datasets that include 280
 # users are loaded from these electrode configurations:
 # 8 Electrode configuration: ['F3', 'C3', 'P3', 'Cz', 'Pz', 'F4', 'C4', 'P4']
-# 16 Electrode configuration: ['F7', 'F3', 'T7', 'C3', 'P7', 'P3', 'O1', 'Cz', 
+# 16 Electrode configuration: ['F7', 'F3', 'T7', 'C3', 'P7', 'P3', 'O1', 'Cz',
 #             'Pz', 'F8', 'F4', 'T8', 'C4', 'P8', 'P4', 'O2']
 pretrained = True  # Parameter to load pre-trained weight values
 
-# Select the number of channels of your application
+# Select the number of channels of your application (8/16 for pretrained=True)
 ncha = 8
 
-# Initialize the model
+# Load the X EEG features and Y labels of your choice in the following format:
+# X = Features in the form of [examples, samples, ncha, 1]
+examples = 1000
+samples = 128 * 3  # frequency = 128 Hz * time= 3 seconds
+# Random signal for demonstrative purposes
+X = np.random.normal(loc=0, scale=1, size=[examples, samples, ncha, 1])
+# Y = one_hot encoded labels, i.e., [examples, 1, 1]
+Y = np.random.randint(low=0,high=2,size=examples)
+Y = to_categorical(Y)
+
+
+# Divide the features into training, validation and test to obtain:
+# X_train, Y_train, X_validate, Y_validate, X_test, Y_test
+X_train = X[:int(0.6*examples)]
+Y_train = Y[:int(0.6*examples)]
+X_validate = X[int(0.6*examples):int(0.8*examples)]
+Y_validate = Y[int(0.6*examples):int(0.8*examples)]
+X_test = X[int(0.8*examples):]
+Y_test = Y[int(0.8*examples):]
+
+#%% Initialize the model
 hyperparameters = dict()
-hyperparameters["ncha"] = channels
+hyperparameters["ncha"] = ncha
 hyperparameters["dropout_rate"] = 0.4
 hyperparameters["activation"] = 'elu'
 hyperparameters["n_classes"] = 2
-hyperparameters["learning_rate"] = 0.0001  # 1e-3 for pretraining and 1e-4 
+hyperparameters["learning_rate"] = 0.0001  # 1e-3 for pretraining and 1e-4
 # for fine-tuning
 hyperparameters["fs"] = 128
-hyperparameters["input_time"] = 3000
+hyperparameters["input_time"] = 3*1000
 hyperparameters["scales_time"] = np.tile([125, 250, 500], 1)
 hyperparameters['filters_per_branch'] = 24
-hyperparameters['ch_lateral'] = (ncha - 2) / 7  # 3/7 For 8/16 electrodes 
+hyperparameters['ch_lateral'] = int((ncha / 2) - 1)  # 3/7 For 8/16 electrodes
 # respectively. For the configurations present in the publication.
-model_hyparams['residual'] = True
-model_hyparams['symmetric'] = True
+hyperparameters['residual'] = True
+hyperparameters['symmetric'] = True
 
-model = EEGSymv2(**model_hyparams)
+model = EEGSym(**hyperparameters)
 model.summary()
 
 # Select if the data augmentation is performed
@@ -59,23 +75,26 @@ if pretrained:
     model.load_weights('EEGSym_pretrained_weights_{}_electrode.h5'.format(ncha))
 
 # Early stopping
-early_stopping = [(kerasEarlyStopping(mode='auto', monitor='val_loss', 
-                   min_delta=0.001, patience=25, verbose=1, 
+early_stopping = [(kerasEarlyStopping(mode='auto', monitor='val_loss',
+                   min_delta=0.001, patience=25, verbose=1,
                    restore_best_weights=True))]
+#%% OPTIONAL: Train the model
+if pretrained:
+    for layer in model.layers[:-1]:
+        layer.trainable = False
 
-# Train the model
-fittedModel = model.fit(trial_iterator(X_train, Y_train, 
-                        batch_size=bs_EEGSym, shuffle=True, 
-                        augmentation=augmentation), 
-                        steps_per_epoch=X_train.shape[0] / bs_EEGSym, 
-                        epochs=500, validation_data=(X_validate, Y_validate), 
-                        callbacks= [early_stopping])
+fittedModel = model.fit(trial_iterator(X_train, Y_train,
+                        batch_size=bs_EEGSym, shuffle=True,
+                        augmentation=augmentation),
+                        steps_per_epoch=X_train.shape[0] / bs_EEGSym,
+                        epochs=500, validation_data=(X_validate, Y_validate),
+                        callbacks=[early_stopping])
 
-# Obtain the accuracies of the trained model
+#%% Obtain the accuracies of the trained model
 probs_test = model.predict(X_test)
 pred_test = probs_test.argmax(axis=-1)
-accuracy = (pred_test == Y_test.argmax(axis=-1)) 
+accuracy = (pred_test == Y_test.argmax(axis=-1))
 
-# Optional: Store the weights
-model.save_weights("\cutom_path")   
+#%% Optional: Store the weights
+model.save_weights("\custom_path")
 ```
